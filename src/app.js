a000aee476cf8f6f6a5b35079ea7c71916421c6d
NateRobinson <840501291@qq.com>
2019-07-16 18:01:05 +0800

    finish build local develop env

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import App from "./app.vue";
 import router from "./router";
 
+// 引入示例模块
+import print from "./print";
+print();
+
 let p = new Promise(function(resolve, reject) {
 	setTimeout(() => {
 		resolve("done");
@@ -11,8 +15,19 @@ p.then(res => {
 	console.log(res);
 });
 
+// 新增代码，测试观察模式。
+console.log("hello world111");
+
 new Vue({
 	el: "#app",
 	router,
 	render: h => h(App),
 });
+
+// 增加对HMR的实现
+if (module.hot) {
+  module.hot.accept("./print.js", function() {
+    console.log("接收更新后的模块");
+    print();
+  });
+}
